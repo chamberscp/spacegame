cd49a35916242f8714cc91599a8e56bc6f8bc5af
Chris Chambers <christopher.p.chambers@gmail.com>
2022-11-21 17:35:51 -0500

    Add Login Section and Introduction Text

--- spacegame.py
+++ spacegame.py
@@ -5,4 +5,23 @@ from   pyfiglet   import Figlet
 f = Figlet(font='doom')
 print(f.renderText('SPACE GAME'))
 
+print(" Welcome explorer.  What is your name?")
 
+# Introduction Section
+def getIntro():
+    print("You find yourself looking into the stars...")
+ 
+
+#Login Section.  Requires the user to login (case sensative). 
+user_list = ["Elliot", "Chris", "Brandon"]
+user_name = input()
+while user_name in user_list:
+    print(f"Welcome " + user_name)
+    getIntro()
+    break
+else:
+    print("Unauthorized user.  Please enter valid username: ")
+    quit()            
+    
+
+    
