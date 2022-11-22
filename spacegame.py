9f6a58a99f29e465bbc38444cb12c53ba409d64c
Chris Chambers <christopher.p.chambers@gmail.com>
2022-11-21 21:48:01 -0500

    Install pygame with background and music

--- spacegame.py
+++ spacegame.py
@@ -1,13 +1,34 @@
-import sys
-import os
-import random
-import textwrap
-import cmd
-import time
+import  pygame
+from    pygame.locals   import *
+from    pygame          import mixer
+from    pyfiglet        import Figlet
 
-from   pyfiglet   import Figlet
-
+# Global variables
 user_name = []
+
+#pygame setup
+pygame.init()
+width = 2000
+height = 2000
+window = pygame.display.set_mode((width,height))
+bg_img = pygame.image.load('space.png')
+bg_img = pygame.transform.scale(bg_img,(width,height))
+
+mixer.init()
+mixer.music.load('desolateworld.ogg')
+mixer.music.play()
+
+pygame.display.set_caption('SPACE GAME')
+
+running = True
+while running:
+    window.blit(bg_img,(0,0))
+    for event in pygame.event.get():
+        if event.type == quit:
+            running = False
+    pygame.display.update()
+pygame.quit()
+
 
 
 #Figlet displays the  in "big" wordart format
