e5eed28f8a2a72f718748a935ae0c72941e904a8
Chris Chambers <christopher.p.chambers@gmail.com>
2022-11-27 20:18:59 -0500

    Add image files to images folder

---
+++ images/city_object.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="city_object" tilewidth="128" tileheight="128" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../spacegame_images/city.svg"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="104" source="../spacegame_images/shuttle.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../../Git/spacegame/images/spacestation.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../../Git/spacegame/images/diamond.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../../Git/spacegame/images/cave.png"/>
+ </tile>
+</tileset>
