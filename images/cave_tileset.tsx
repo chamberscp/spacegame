96b44943c7b70fac4e62e66be1d1f84cbf8c31a9
Chris Chambers <christopher.p.chambers@gmail.com>
2022-11-28 10:24:45 -0500

    Add new tilesets and enewearth.tmx

---
+++ images/cave_tileset.tsx
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="cave" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="cave.png" width="128" height="128"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32.1875" y="27.25">
+    <polygon points="0,0 -4.8125,4.3125 -0.375,4.8125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="23.8125" y="0.1875">
+    <polygon points="0,0 -23.8125,26.6875 -23.625,31.625 8.125,31.8125 7.625,6.4375"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.1875" y="5.375">
+    <polygon points="0,0 8.1875,0.5 31.625,8.375 31.75,26.3125 -0.125,26.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.125" y="14.25">
+    <polygon points="0,0 19.25,17.625 0.1875,17.625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.0625" y="0.375">
+    <polygon points="0,0 -8.0625,17.0625 -11.125,29.3125 -13,31.1875 16.8125,31.625 16.875,-0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="20.1875" y="0.375">
+    <polygon points="0,0 8.625,20.75 10.125,31.0625 -20.125,31.625 -19.9375,-0.1875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.125" y="0.0625" width="30.9375" height="31.9375"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.3125" y="0.25" width="31.0625" height="31.8125"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.75" y="0.1875">
+    <polygon points="0,0 2,1.8125 1.5625,0 26.8125,0.0625 24.625,16.8125 18.5625,31.625 -4.8125,31.4375 -4.5625,-0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.875" y="0">
+    <polygon points="0,0 0.375,9.4375 16.75,9.5 30.875,21.8125 31,0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.25" y="22">
+    <polygon points="0,0 25.125,9.9375 31.75,9.6875 31.625,-21.8125 -0.125,-21.875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.125" y="31.6875">
+    <polygon points="0,0 9.75,0.0625 31.5,-8.125 31.5625,-31.375 0.3125,-31.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="23.3125">
+    <polygon points="0,0 23.0625,-22.8125 0.3125,-23.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
