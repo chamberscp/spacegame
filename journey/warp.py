60b23b3945ea57e1eca6b1e017e049ecf86d01a9
Chris Chambers <christopher.p.chambers@gmail.com>
2022-11-30 17:32:48 -0500

    Add getTime function

--- journey/warp.py
+++ journey/warp.py
@@ -1,13 +1,8 @@
 from fractions import Fraction
 
-c = 299792458
+c = 670616629
+mph = 0
 
-c_per_day = float(2.348336594911937)
-c_per_hour = float(0.0978473581213071)
-lightyear_hours = float(10.22)
-days_per_year = 365
-hours_per_day = float(24.00)
-hours_per_year = 8760
 
 
 a = 'Alpha_Centari'     
@@ -15,40 +10,41 @@ Aplha_Centari = 4.29        # Has 3 known planets
 b = 'Epsillon_Eridani'
 Epsillon_Eridanio = 10.50     # Has 1 known planet
 c = 'Wolf1061' 
-Wolf_1061 = 13.8             # 3 planmets, 1 in habitable zone
+Wolf_1061 = 13.8             # 3 planets, 1 in habitable zone
 d= 'HR3259'
 HR3259 = 41.04               # Has 3 known planets
 
-time = 0
-warp = 0
 v = 0
 
 #Calculate velocity in m/s
 
 def getVelocity():
-    global warp
     global v
+    global mph
     var1 = input('Please enter the warp speed (0-10) that you would like to go (and keep in mind that the faster you will go you will burn more fuel, but age less): ')
     var1 = Fraction(var1)
-    expo = Fraction('10/3')
+    expo = Fraction(10/3)
     result = var1**expo
-    print(result)
     if v == 0:
         v = result * 299792458
-        print(v)
         print(f'Velocity is equal to {v} m/s')
+        mph = v*2.237
+        print(f'{mph} mph')
+        print('time in hours = distance in miles / vel ')
 
 
 def getTime():
+    global v
+    global mph
     m = input('Where would you like to go? ')
     print(m)
-    global v
     if m == '1':
-        dist = .102
-        #print(f'{a} is {dist} light years away.')
-        #getVelocity()
-        time = dist * v
-        #print(f'It will take {time} days to get to {a}')
+        dist = 2.54*(10**13)
+        print(f'{a} is {dist} light years away.')
+        getVelocity()
+        time = dist / mph
+        days = time/24
+        print(f'It will take {days} days to get to {a}')
       
     '''elif m == '2':
         x = Epsillon_Eridani
@@ -62,7 +58,7 @@ def getTime():
 
 getTime()
       
-       
+      
        
      
 
